9983d2ffd365cc5115a662181efa194e5864df14
Azizbek Asadov <azizbek1998@icloud.com>
2023-01-18 17:04:21 +0500

    Initial Commit

---
+++ CountryCodes/PresentationLayer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CountryCodes
+//
+//  Created by Azizbek Asadov on 18/01/23.
+//
+
+import UIKit
+
+final class CountryListViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
